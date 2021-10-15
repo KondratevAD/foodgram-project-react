9ab84884733514f2e98c1a814eb67bdb80c589c9
KondratevAD <Kondratev_ad@bk.ru>
2021-10-15 23:15:48 +0300

    del

--- backend/grocery_assistant/tests/api/constants_url.py
+++
@@ -1,6 +0,0 @@
-# User
-users = '/api/users/'
-users_me = '/api/users/me/'
-user_follow_list = '/api/users/subscriptions/'
-recipes = '/api/recipes/'
-
--- backend/grocery_assistant/tests/settings_qa.py
+++
@@ -1,8 +0,0 @@
-from grocery_assistant.settings import *
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
